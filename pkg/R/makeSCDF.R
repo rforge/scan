b944a450b286399ffd3eec62ab8f0efdbb7e15a5
jazznbass <jazznbass@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-14 12:44:03 +0000

    cleaned up makeSCDF and .scpreparedata
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/scan@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/makeSCDF.R
+++ pkg/R/makeSCDF.R
@@ -1,7 +1,52 @@
 
 makeSCDF <- function (data, B.start = NULL, MT = NULL){
-  out <- .SCprepareData(data = data, B.start = B.start, MT = MT)
-  if(length(out) == 1)
-    return(out[[1]])
-  return(out)
+
+  
+  #vector with B.start -> single data frame
+  
+  if (class(data) == "numeric" || class(data) == "integer") {
+    if (is.null(MT))
+      MT <- 1:length(data)
+    B.start <- which(MT == B.start)
+    D <- c(rep("A", B.start - 1), rep("B", length(data) - B.start + 1))
+    data <- data.frame(phase = D, values = data, mt = MT)
+  }
+  
+  #single data.frame -> list with single data frame
+  #if (class(data) == "data.frame") {
+  if(is.data.frame(data)) {
+    if(ncol(data) == 2)
+      data[3] <- 1:nrow(data)
+    names(data) <- c("phase", "values", "mt")
+    data <- list(data)
+  }
+  
+  #list with vectors and vector with B.start -> list with data frames
+  if(length(B.start) > 1) {
+    if(length(data) != length(B.start) || mode(data) != "list")
+      stop("Wrong data format. Format schould be a list of vectors and a vector of startpoints.")
+    dat <- list()
+    for(i in 1:length(data)) {
+      if (is.null(MT))
+        MT.tmp <- 1:length(D)
+      else
+        MT.tmp <- MT[[i]]
+      B.start[i] <- which(MT.tmp == B.start[i])
+      D <- c(rep("A", B.start[i] - 1), rep("B", length(data[[i]]) - B.start[i] + 1))
+      dat[[i]] <- data.frame(phase = D, values = data[[i]], mt = MT.tmp)
+    }
+    data <- dat
+  }
+  
+  if(class(data) != "list")
+    stop("Wrong data format. Data have to be a numeric vector, a data.frame, or a list.")
+  
+  
+  for(i in 1:length(data))
+    if(ncol(data[[i]]) == 2) data[[i]]$mt <- 1:nrow(data[[i]])
+    
+    if(length(data) == 1)
+      return(data[[1]])
+    return(data)
+
 }
